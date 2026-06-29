01a4ddd3b7c5a2a8c3b4d02ea21d9609bc7b7059
Aditya Gupta <adityagupta2087@gmail.com>
2026-06-29 13:29:14 +0530

    shifted to end-4 hyprland.

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,35 @@
+# Commands to run in interactive sessions can go here
+if status is-interactive
+    # No greeting
+    set fish_greeting
 
-set fish_greeting
-
-
-export BUN_INSTALL="$HOME/.bun"
-export GO_BIN="$HOME/go/bin"
-export PATH="$BUN_INSTALL/bin:$PATH"
-export PATH="$GO_BIN:$PATH"
-
-alias ls 'eza --icons'
-alias c "printf '\033[2J\033[3J\033[1;1H'"
-alias t "touch"
-alias bat 'bat --theme 1337'
-alias fzf "fzf --preview 'bat --theme 1337 --style=numbers --color=always --line-range :500 {}' | wl-copy"
-alias vim 'nvim'
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-
-
-# Added by Antigravity CLI installer
-set -gx PATH "/home/aditya/.local/bin" $PATH
+    # Use starship
+    function starship_transient_prompt_func
+        starship module character
+    end
+    if test "$TERM" != "linux"
+        starship init fish | source
+        enable_transience
+    end
+    
+    # Colors
+    if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+        cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
+    
+    alias ls 'eza --icons'
+    alias c "printf '\033[2J\033[3J\033[1;1H'"
+    alias t "touch"
+    alias bat 'bat --theme 1337'
+    alias fzf "fzf --preview 'bat --theme 1337 --style=numbers --color=always --line-range :500 {}' | wl-copy"
+    alias vim 'nvim'
+    # Aliases
+    # kitty doesn't clear properly so we need to do this weird printing
+    # alias q 'qs -c ii'
+    if test "$TERM" != "linux"
+        alias ls 'eza --icons'
+    end
+    if test "$TERM" = "xterm-kitty"
+        alias ssh 'kitten ssh'
+    end
+end
--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,6 @@ if not vim.uv.fs_stat(lazypath) then
   local repo = "https://github.com/folke/lazy.nvim.git"
   vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 local lazy_config = require "configs.lazy"
